62e726332ea22cadbc1c1876f813f6c96dde37cb
LeTamanoir <martin.saldinger@kiln.fi>
2025-10-06 23:44:20 +0200

    merge structs into CallCtx

---
+++ src/CallCtx.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Iris;
+
+use Google\Protobuf\Internal\Message;
+
+/**
+ * Call context for a gRPC call.
+ */
+class CallCtx
+{
+    /**
+     * The encoding to use for the request.
+     */
+    public Encoding $enc = Encoding::Identity;
+
+    /**
+     * The request message.
+     */
+    public Message $args;
+
+    /**
+     * The method to call.
+     */
+    public string $method;
+
+    /**
+     * The ID of the call.
+     */
+    public string $id;
+
+    /**
+     * The curl options to use for the request.
+     *
+     * @var array<int, int|string|bool>
+     */
+    public array $curlOpts = [];
+
+    /**
+     * The timeout for the request in milliseconds.
+     */
+    public int $timeoutMs = 30_000;
+
+    /**
+     * The user agent to use for the request.
+     */
+    public string $userAgent = 'iris-php/' . \Iris\VERSION;
+
+    /**
+     * Additional metadata for the call.
+     *
+     * @var array<string, mixed>
+     */
+    public array $meta = [];
+}
